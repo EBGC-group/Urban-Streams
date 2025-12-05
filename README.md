913a032c0db3356c5bdcf802767157e1e6b7f2e7
Jim Junker <james.junker1@gmail.com>
2025-12-05 11:31:01 -0600

    update meteorological data [no actions]

--- README.md
+++ README.md
@@ -1,21 +1,11 @@
 
 <!-- README.md is generated from README.Rmd. Please edit that file -->
 
-# project-template
+# Urban Streams
 
 <!-- badges: start -->
 
 <!-- badges: end -->
 
-The goal of project-template is to make it easy to create a new project
-with a website to allow for tutorials, project logs, protocols, etc.
-This template can be used for new projects to standardize project
-structure.
-
-The template is build around a Quarto website to allow github pages to
-be source from `docs/`. The Quarto website is configured in the
-`_quarto.yml` file with the `output-dir: docs` command.
-
-The layout of the website is also set in the `quarto.yml` document.
-Website structure and additional pages can be added by modifying this
-file.
+This project organizes the workflow for project related to Urban Streams
+within the EBGC group at UNT.
---
+++ code/get-merge-met-data.R
@@ -0,0 +1,43 @@
+# This script is used to automate the updating of meteorological data. Currently, the script downloads data for:
+# Denton Municipal Airport. Station ID:USW00003991
+# ...add other stations as necessary...
+
+# load relevant packages
+library(here)
+here::i_am('code/get-merge-met-data.R')
+library(magrittr)
+library(dplyr)
+library(tidyr)
+library(worldmet)
+
+# determine what the current year is to ensure we download the minimal amount of data given the constraints of the functions 
+current_year = format(Sys.Date(), "%Y")
+
+# load the current data file in /data folder
+
+current_met_DMA = read.table(here::here("data/met_DMA.txt"), header = TRUE,  sep = '\t')
+
+# download the updated meteorological data and subset columns
+
+new_met_DMA = import_ghcn_hourly(station = "USW00003991",
+                             year = current_year,
+                             extra = TRUE) %>% 
+  dplyr::select(station_id, station_name, date, air_temp, atmos_pres, sea_pres, altimeter, precip)
+
+# merge the old and new data sets and save
+
+updated_met_DMA = merge(current_met_DMA, new_met_DMA)
+
+write.table(updated_met_DMA,
+            file = here::here("data/met_DMA.txt"),
+            row.names = FALSE,
+            quote = FALSE,
+            sep = '\t',
+            overwrite = TRUE)
+
+# create a timestamp of last successful run
+
+saveRDS(format(Sys.Date(), "%Y-%m-%d %H:%M:%S"), here::here("data/met_DMA_timestamp.rds"))
+
+
+
