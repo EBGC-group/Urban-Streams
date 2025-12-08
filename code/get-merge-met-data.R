5ed15ba67a906683637898fe93a99c044d8f74fa
Jim Junker <james.junker1@gmail.com>
2025-12-08 14:09:34 -0600

    update data file to csv and setup to append new data rather than rewrite whole file. [skip ci]

--- code/get-merge-met-data.R
+++ code/get-merge-met-data.R
@@ -8,14 +8,16 @@ here::i_am('code/get-merge-met-data.R')
 library(magrittr)
 library(dplyr)
 library(tidyr)
+library(readr)
 library(worldmet)
+'%ni%' <- Negate('%in%')
 
 # determine what the current year is to ensure we download the minimal amount of data given the constraints of the functions 
 current_year = format(Sys.Date(), "%Y")
 
 # load the current data file in /data folder
 
-current_met_DMA = read.table(here::here("data/met_DMA.txt"), header = TRUE,  sep = '\t')
+current_met_DMA = read_csv(here::here("data/met_DMA.csv"))
 
 # download the updated meteorological data and subset columns
 
@@ -24,16 +26,14 @@ new_met_DMA = import_ghcn_hourly(station = "USW00003991",
                              extra = TRUE) %>% 
   dplyr::select(station_id, station_name, date, air_temp, atmos_pres, sea_pres, altimeter, precip)
 
-# merge the old and new data sets and save
+# merge the old and new data sets. Identify new date-times and append them to the old file
 
-updated_met_DMA = merge(current_met_DMA, new_met_DMA)
+updated_met_DMA = new_met_DMA[which(new_met_DMA$date %ni% current_met_DMA$date),]
 
-write.table(updated_met_DMA,
-            file = here::here("data/met_DMA.txt"),
-            row.names = FALSE,
-            quote = FALSE,
-            sep = '\t',
-            append = FALSE)
+write_csv(updated_met_DMA,
+            file = here::here("data/met_DMA.csv"),
+            quote = "none",
+            append = TRUE)
 
 # create a timestamp of last successful run
 
