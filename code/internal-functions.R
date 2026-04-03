91c755bc1fa261b9cf6803d42123718f6b1102af
Jim Junker <james.junker1@gmail.com>
2026-04-03 12:56:27 -0500

    started internal functions code.

---
+++ code/internal-functions.R
@@ -0,0 +1,25 @@
+#'
+#'
+#'
+#'
+read_clean_DO_files = function(filepath = NULL,...){
+  
+  
+}
+
+
+#'
+#'
+#'
+read_clean_depth_files = function(filepath = NULL){
+  
+  
+}
+
+#'
+#'
+#'
+read_clean_cond_files = function(filepath = NULL){
+  
+  
+}
